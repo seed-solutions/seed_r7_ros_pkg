2b36820c2eeb0a5fb64524729cb7d25c84dd002d
hi-kondo <hi.kondo@thk.co.jp>
2019-07-11 17:26:15 +0900

    Development (#14)
    * package.xml version updated
    
    * package.xml version update
    
    * updated
    
    * update package.xml
    
    * delete some directories
    
    * package.xml tag changed
    
    * updated
    
    * fixes
    
    * renew CHANGELOG
    
    * 0.0.2
    
    * fixed
    
    * 0.0.3
    
    * update
    
    * update
    
    * 0.0.4
    
    * updated
    
    * updated
    
    * 0.0.5
    
    * added stroke_conveeter
    
    * update interface
    
    * updated CMakeLists.txt
    
    * updated
    
    * local run_test fixed
    
    * delete Anglejointnames
    
    * separate RobotController to upper&lower and include seed_solutions_sdk
    
    * update ros_controller
    
    * update noid_bringup.launch
    
    * update CMaleLists and package.xml
    
    * noid?hand_controller update
    
    * update mover-controller
    
    * hand_controll update
    
    * noid_hand_controller updated
    
    * debug fix
    
    * model name change
    
    * delete some of files
    
    * wheel_test_update

--- noid_robot_interface/src/robot_stroke_converter.cpp
+++ noid_robot_interface/src/robot_stroke_converter.cpp
@@ -139,7 +139,148 @@ void Stroke2Angle
 
 }
 
+namespace typefcety
+{
+   //////////////////////////////////////////////////
+    void Angle2Stroke
+    (std::vector<int16_t>& _strokes, const std::vector<double> _angles)
+    {
+      float rad2Deg = 180.0 / M_PI;
+      float scale = 100.0;
+      dualJoint right_wrist =
+        WristRollPitchTable(rad2Deg * _angles[23],
+                            rad2Deg * _angles[24]);
+      dualJoint left_wrist =
+        WristRollPitchTable(rad2Deg * _angles[8],
+                            -rad2Deg * _angles[9]);
+      dualJoint waist =
+        WaistRollPitchTable(-rad2Deg * _angles[2],
+                            rad2Deg * _angles[1]);
+      dualJoint neck =
+        NeckRollPitchTable(rad2Deg * _angles[17],
+                           rad2Deg * _angles[16]);
 
+      // ros_order -> can_order
+      _strokes[0] = scale * rad2Deg * _angles[15];
+      _strokes[1] = scale * neck.one;
+      _strokes[2] = scale * neck.two;
+
+      _strokes[3] =
+        scale * ShoulderPitchTable(-rad2Deg * _angles[18]);
+      _strokes[4] =
+        scale * ShoulderRollTable(-rad2Deg * _angles[19]);
+      _strokes[5] = -scale * rad2Deg * _angles[20];
+      _strokes[6] = scale * ElbowPitchTable(-rad2Deg * _angles[21]);
+      _strokes[7] = -scale * rad2Deg * _angles[22];
+      _strokes[8] = scale * right_wrist.one;
+      _strokes[9] = scale * right_wrist.two;
+      _strokes[10] = -scale * rad2Deg * _angles[25];
+      _strokes[11] = -scale * (rad2Deg * _angles[29] - 50.0) * 0.18;
+
+      _strokes[12] =
+        scale * ShoulderPitchTable(-rad2Deg * _angles[3]);
+      _strokes[13] =
+        scale * ShoulderRollTable(rad2Deg * _angles[4]);
+      _strokes[14] = -scale * rad2Deg * _angles[5];
+      _strokes[15] = scale * ElbowPitchTable(-rad2Deg * _angles[6]);
+      _strokes[16] = -scale * rad2Deg * _angles[7];
+      _strokes[17] = scale * left_wrist.one;
+      _strokes[18] = scale * left_wrist.two;
+      _strokes[19] = -scale * rad2Deg * _angles[10];
+      _strokes[20] = scale * (rad2Deg * _angles[14] + 50.0) * 0.18;
+
+      _strokes[21] = scale * waist.two;
+      _strokes[22] = scale * waist.one;
+      _strokes[23] = scale * rad2Deg * _angles[0];
+
+      _strokes[24] = scale * LegTable(rad2Deg * _angles[31]);
+      _strokes[25] = scale * LegTable(- rad2Deg * _angles[30]);
+    }
+
+       //////////////////////////////////////////////////
+    void Stroke2Angle
+    (std::vector<double>& _angles, const std::vector<int16_t> _strokes)
+    {
+      float scale = 0.01;
+      float left_wrist_roll_stroke =
+        (scale * _strokes[17] + scale * _strokes[18]) * 0.5;
+      float right_wrist_roll_stroke =
+        (scale * _strokes[8] + scale * _strokes[9]) * 0.5;
+      float waist_pitch_stroke =
+        (scale * _strokes[21] + scale * _strokes[22]) * 0.5;
+      float neck_pitch_stroke =
+        (scale * _strokes[1] + scale * _strokes[2]) * 0.5;
+      float deg2Rad = M_PI / 180.0;
+      float knee_angle  = - deg2Rad * LegInvTable(scale * _strokes[25]);
+      float ankle_angle = deg2Rad * LegInvTable(scale * _strokes[24]);
+
+      // can_order -> ros_order
+      _angles[0] =
+        deg2Rad * scale * _strokes[23];
+      _angles[1] =
+        deg2Rad * WaistPitchInvTable(waist_pitch_stroke);
+      _angles[2] =
+        deg2Rad * WaistRollInvTable(scale * _strokes[21] - waist_pitch_stroke);
+
+      _angles[3] =
+        -deg2Rad * ShoulderPitchInvTable(scale * _strokes[12]);
+      _angles[4] =
+        deg2Rad * ShoulderRollInvTable(scale * _strokes[13]);
+      _angles[5] =
+        -deg2Rad * scale * _strokes[14];
+      _angles[6] =
+        -deg2Rad * ElbowPitchInvTable(scale * _strokes[15]);
+      _angles[7] =
+        -deg2Rad * scale * _strokes[16];
+      _angles[8] =
+        deg2Rad * WristPitchInvTable(scale * _strokes[17] - left_wrist_roll_stroke);
+      _angles[9] =
+        -deg2Rad * WristRollInvTable(left_wrist_roll_stroke);
+      _angles[10] =
+        -deg2Rad * scale * _strokes[19];
+      _angles[11] =
+        -deg2Rad * (scale * _strokes[20] * 5.556 - 50.0);
+      _angles[12] = 0;
+      _angles[13] = 0;
+      _angles[14] =
+        deg2Rad * (scale * _strokes[20] * 5.556 - 50.0);
+
+      _angles[15] =
+        deg2Rad * scale * _strokes[0];
+      _angles[16] =
+        deg2Rad * NeckPitchInvTable(neck_pitch_stroke);
+      _angles[17] =
+        -deg2Rad * NeckRollInvTable(scale * _strokes[1] - neck_pitch_stroke);
+
+      _angles[18] =
+        -deg2Rad * ShoulderPitchInvTable(scale * _strokes[3]);
+      _angles[19] =
+        -deg2Rad * ShoulderRollInvTable(scale * _strokes[4]);
+      _angles[20] =
+        -deg2Rad * scale * _strokes[5];
+      _angles[21] =
+        -deg2Rad * ElbowPitchInvTable(scale * _strokes[6]);
+      _angles[22] =
+        -deg2Rad * scale * _strokes[7];
+      _angles[23] =
+        deg2Rad * WristPitchInvTable(scale * _strokes[8] - right_wrist_roll_stroke);
+      _angles[24] =
+        deg2Rad * WristRollInvTable(right_wrist_roll_stroke);
+      _angles[25] =
+        -deg2Rad * scale * _strokes[10];
+      _angles[26] =
+        deg2Rad * (scale * _strokes[11] * 5.556 - 50.0);
+      _angles[27] = 0;
+      _angles[28] = 0;
+      _angles[29] =
+        -deg2Rad * (scale * _strokes[11] * 5.556 - 50.0);
+
+      _angles[30] = knee_angle;
+      _angles[31] = ankle_angle;
+    }
+
+
+}
 
 
 
